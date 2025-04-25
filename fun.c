3ace1466e358c004964e60dbad9bdb4978055c4b
47ghost <dd2005040302@gmail.com>
2025-04-25 15:36:53 +0800

    new-feature hellio

--- fun.c
+++ fun.c
@@ -1,5 +1,9 @@
 void fun(){
     printf("Hello,world!");
     printf("day1work (in new feature)");
+<<<<<<< HEAD
     printf("master hello");
+=======
+    printf("new-feature hello");
+>>>>>>> 41abb93 (new-feature hellio)
 }
