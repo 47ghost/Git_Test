d89bfeed49b6f8ba621739b47f62138d0dd40e65
47ghost <dd2005040302@gmail.com>
2025-04-25 16:17:12 +0800

    update test02

--- fun.c
+++ fun.c
@@ -2,7 +2,7 @@ void fun(){
     printf("Hello,world!");
     printf("day1work (in new feature)");
     printf("master hello");
-    printf("hello in test02");
+    printf("hello in test02 update");
 
 
 }
