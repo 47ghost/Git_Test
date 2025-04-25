6686e03d2a0686b8cba91fd2df1dacc9d2eed123
47ghost <dd2005040302@gmail.com>
2025-04-25 16:04:34 +0800

    update02

--- fun.c
+++ fun.c
@@ -1,9 +1,6 @@
 void fun(){
     printf("Hello,world!");
     printf("day1work (in new feature)");
-<<<<<<< HEAD
     printf("master hello");
-=======
     printf("new-feature hello");
->>>>>>> 41abb93 (new-feature hellio)
 }
