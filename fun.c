ede83293727299f89016dffdd27df7466c1594bc
47ghost <dd2005040302@gmail.com>
2025-04-25 16:13:19 +0800

    hello in test02

--- fun.c
+++ fun.c
@@ -2,5 +2,7 @@ void fun(){
     printf("Hello,world!");
     printf("day1work (in new feature)");
     printf("master hello");
-    printf("new-feature hello");
+    printf("hello in test02");
+
+
 }
